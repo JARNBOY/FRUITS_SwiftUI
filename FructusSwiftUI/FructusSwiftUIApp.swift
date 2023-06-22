55a7d565ab5b0552b66923589d80ce913f6c775d
Papon <papon.s@lawyerplusgroup.com>
2023-06-22 09:57:09 +0700

    Initial Commit

---
+++ FructusSwiftUI/FructusSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusSwiftUIApp.swift
+//  FructusSwiftUI
+//
+//  Created by Papon Supamongkonchai on 22/6/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct FructusSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
