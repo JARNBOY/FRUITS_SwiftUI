44c7d49fdf81b64053b703e9c0c5fcae2d10dd90
Papon <papon.s@lawyerplusgroup.com>
2023-06-22 20:13:08 +0700

    [FRUITS App Workshop] Learn how to create data model and fetch data using Swift (Reviewed by Papon S.)

--- FructusSwiftUI/View/FruitCardView.swift
+++ FructusSwiftUI/View/FruitCardView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct FruitCardView: View {
     //MARK: - PROPERTY
+    var fruit: Fruit
     @State private var isAnimating: Bool = false
-    
     //MARK: - BODY
     var body: some View {
         
@@ -18,21 +18,21 @@ struct FruitCardView: View {
             
             VStack {
                 // FRUIT: IMAGE
-                Image("blueberry")
+                Image(fruit.image)
                     .resizable()
                     .scaledToFit()
                     .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
                     .scaleEffect(isAnimating ? 1.0 : 0.6)
                 
                 // FRUIT: TITLE
-                Text("Blueberry")
+                Text(fruit.title)
                     .foregroundColor(.white)
                     .font(.largeTitle)
                     .fontWeight(.heavy)
                     .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
                 
                 // FRUIT: HEADLINE
-                Text("Blueberries are sweet, nutritious and wildly popular fruit all over the world.")
+                Text(fruit.headline)
                     .foregroundColor(Color.white)
                     .multilineTextAlignment(.center)
                     .padding(.horizontal, 16)
@@ -42,17 +42,16 @@ struct FruitCardView: View {
                 StartButtonView()
             }//: VStack
         }//: ZStack
-        .onAppear(perform: {
-            withAnimation( .easeOut(duration: 0.5)) {
-                isAnimating = true
-            }
-        })
+        .onAppear {
+            isAnimating = false
+          withAnimation(.easeOut(duration: 0.5)) {
+            isAnimating = true
+          }
+        }
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0 , maxHeight: .infinity, alignment: .center)
         .background(
             LinearGradient(
-                gradient: Gradient(colors: [
-                    Color("ColorBlueLight"),Color("ColorBlueDark")
-                ]),
+                gradient: Gradient(colors: fruit.gradientColors),
                 startPoint: .top,
                 endPoint: .bottom)
         )
@@ -63,7 +62,7 @@ struct FruitCardView: View {
 
 struct FruitCardView_Previews: PreviewProvider {
     static var previews: some View {
-        FruitCardView()
+        FruitCardView(fruit: fruitsData[1])
             .previewLayout(.fixed(width: 320, height: 640))
     }
 }
